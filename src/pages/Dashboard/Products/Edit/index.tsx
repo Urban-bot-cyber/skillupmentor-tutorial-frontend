f1301266edb61e978bf59c5c5ed0a680d14aa253
Urban Prosenjak <prosenjak.urban@gmail.com>
2024-07-30 08:19:53 +0200

    Products

--- src/pages/Dashboard/Products/Edit/index.tsx
+++ src/pages/Dashboard/Products/Edit/index.tsx
@@ -1,9 +1,15 @@
+import CreateUpdateProductForm from 'components/product/CreateUpdateProductForm/CreateUpdateProductForm'
 import DashboardLayout from 'components/ui/DashboardLayout'
 import { FC } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const DashboardProductsEdit: FC = () => {
+    const location = useLocation()
     return(
-        <DashboardLayout>DashboardProductsEdit</DashboardLayout>
+        <DashboardLayout>
+            <h1 className='mb-4 text-center'>Create new product</h1>
+            <CreateUpdateProductForm defaultValues={location.state}/>
+        </DashboardLayout>
     )
 }
 
